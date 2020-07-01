00468cde9102734c29c29b201720936920d00486
Salaar Syed <salarsyedd@gmail.com>
2020-07-01 04:53:49 -0500

    Staging the header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './Components/Header';
 
-function App() {
-  return (
-    <div className="App">
-     <h1> Hello</h1>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className= "App">
+        <Header/>
+       </div>
+    );
+  }
 }
 
 export default App;
