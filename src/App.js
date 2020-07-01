95cbbc781399a277ff922f216ec437e30a44ff1a
Omar Imran <mughaloimran@gmail.com>
2020-07-01 07:05:59 -0500

    Added headre and footere

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Header from './Components/Header';
 
 class App extends Component {
+
+
   render() {
     return (
       <div className= "App">
         <Header/>
-       </div>
+      </div>
     );
   }
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import '../Components/Header.css';
+import styles from '../Components/Header.module.css';
 
 class Header extends Component {
   render() {
     return (
+      
       <div className= "Header">
-        <h1>Hello</h1>
-        
+        <body>
+          <div className={styles.headre}>
+            <div className={styles.container}>
+              <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Products</a>
+                <a href="#">Services</a>
+                <a href="#">Contact Us</a>
+              </nav>
+            </div>
+          </div>
+          <div className={styles.container}>
+            <p>Welcome to DevState</p>
+          </div>    
+          <div className={styles.footere}>
+            <div className={styles.container}>Copyright; 2020 DevState</div>        
+          </div>
+        </body>
       </div>
+
     );
   }
 }
